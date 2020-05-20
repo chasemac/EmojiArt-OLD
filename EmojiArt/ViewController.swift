02b97d2abbff8ccf218a4b8c875b9df1889b34d9
Chase McElroy <ichasem@gmail.com>
2020-05-20 09:25:58 -0700

    Initial Commit

---
+++ EmojiArt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiArt
+//
+//  Created by Chase McElroy on 5/20/20.
+//  Copyright © 2020 ChaseMcElroy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
